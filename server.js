0856ba0d0e3410d90c12f868e130b186d7e9d37d
Hillary-dot <hillaryhariri@gmail.com>
2026-04-26 19:28:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const Africastalking = require("africastalking")({
 const sms = Africastalking.SMS;
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
